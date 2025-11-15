f68367fccc8b4fc5d3e5328d82928de19d3c8ded
Leidy Collazos <leidycollazos04@gmail.com>
2025-11-15 16:22:04 -0500

    Creación de archivos css y js

---
+++ projectOpFucntions/assets/js/operadores.aritmeticos.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores aritméticos
+ */
+
+let num1 = 8, num2 = 4;
+let res1, res2, res3, res4;
+
+//Suma
+res1 = num1 + num2;
+//Resta
+res2 = num1 - num2;
+//Multiplicación
+res3 = num1 * num2;
+//División
+res4 = num1 / num2;
+
+document.getElementById("demo").innerHTML="Suma: " + res1 + "Resta: " + res2 + "Multiplicación: " + res3 + "División" + res4
